0309f838dee6933b06c7ebad6a8aca227115a240
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-03-26 12:09:37 +0100

    Register form in react working. Need lot improvments

--- src/API/APIHelper.js
+++ src/API/APIHelper.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import { ResponsiveEmbed } from 'react-bootstrap';
 
-function APIHelper(email, login, password) {
-    
-let data;
+export const APIHelper = {
+    Register,
+};
 
+
+function Register(Email, UserName, Password, ConfirmPassword) {
+    
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, login, password })
+        body: JSON.stringify({ Email, UserName, Password, ConfirmPassword })
     };
 
-    return fetch('https://localhost:44342/api/Account/register', requestOptions)
-    .then(handleResponse=>handleResponse.JSON())
-    .then(handleResponse => { data = handleResponse.data
-       
+    let info ="init";
 
-    
-    });
+   fetch('https://localhost:44342/api/Account/register', requestOptions)
+   .then(response => {
+       // reject not ok response
+       if (!response.ok) {
+           return Promise.reject(response)
+       }
+       return response.text() // or return response.text()
+   })
+   // catch error response and extract the error message
+   .catch(async response => {
+       const error = await response.text().then(text => text)
+       if(error == null){
+        info ="server problem!";
+        return info;
+       }
+       return Promise.reject(error)
+   })
+   .then(data => {
+    info ="Succes!"
+    return info;
+   })
+   .catch(error => {
+       //Connection problem
+       if(error =="TypeError: response.text is not a function")
+       {
+        info='Connection problem';
+        return info;
+       }
+       else{
+        info=error
+        return info;
+       }        
+   })
 
-}
-
-
-export default APIHelper
+    };
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, {Component } from 'react'
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Spinner } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import APIHelper from '../API/APIHelper';
+import {APIHelper} from '../API/APIHelper';
 
 class RegisterForm extends Component {
 
@@ -9,9 +9,11 @@ class RegisterForm extends Component {
         super()
         this.state = {
             Email: "",
-            UserName: "",
+            Login: "",
             Password: "",
-            passwordRepeat: "",
+            ConfirmPassword: "",
+            InfoMessage: "",
+            DuringOperation: false
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,18 +27,70 @@ class RegisterForm extends Component {
         this.CanRegister();
     }
 
-    handleSubmit(event) {
-     
-        this.Register(this.state.Email, this.state.UserName, this.state.Password )
+   handleSubmit(event) {
+  
+       
+       this.Register(this.state.Email, this.state.Login, this.state.Password, this.state.ConfirmPassword)
+//let info = APIHelper.Register(this.state.Email, this.state.Login, this.state.Password, this.state.ConfirmPassword);
+
+
+
         event.preventDefault();
       }
+
+
+     Register(Email, UserName, Password, ConfirmPassword) {
+    
+this.setState({DuringOperation : true})
+
+         const requestOptions = {
+             method: 'POST',
+             headers: { 'Content-Type': 'application/json' },
+             body: JSON.stringify({ Email, UserName, Password, ConfirmPassword })
+         };
+
+
+        fetch('https://localhost:44342/api/Account/register', requestOptions)
+        .then(response => {
+            // reject not ok response
+            if (!response.ok) {
+                return Promise.reject(response)
+            }
+            return response.text() // or return response.text()
+        })
+        // catch error response and extract the error message
+        .catch(async response => {
+            const error = await response.text().then(text => text)
+            return Promise.reject(error)
+        })
+        .then(data => {
+            console.log("Succes!")
+            this.setState({InfoMessage:"Succes!" })
+            this.setState({DuringOperation : false})
+        })
+        .catch(error => {
+            //Connection problem
+            if(error =="TypeError: response.text is not a function")
+            {
+                console.log('Connection problem')
+                this.setState({InfoMessage:'Connection problem'})
+                this.setState({DuringOperation : false})
+            }
+            else{
+                console.log(error)
+                this.setState({InfoMessage:error})
+                this.setState({DuringOperation : false})
+            }        
+        })
+
+};
 
      CanRegister()
       {
           let output = false;
-          if(this.state.Email && this.state.UserName && this.state.Password && this.state.passwordRepeat)
+          if(this.state.Email && this.state.Login && this.state.Password && this.state.ConfirmPassword)
           {
-              if(this.state.password === this.state.passwordRepeat)
+              if(this.state.Password === this.state.ConfirmPassword)
               {
                 output = true;
               }
@@ -46,60 +100,80 @@ class RegisterForm extends Component {
           return output;
       }
 
-       Register(Email, UserName, Password) {
-    
-        let data;
-        
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ Email, UserName, Password })
-            };
-        
-            return fetch('https://localhost:44342/api/Account/register', requestOptions)
-            .then(handleResponse=>handleResponse.JSON())
-            .then(handleResponse => { data = handleResponse.data
-               
-        
-            
-            });
-        
-        }
-        
+
+
+      TestCall() { 
+        //  fetch("https://swapi.co/api/people/1")
+        // .then(response => response.json())
+        // .then(data => console.log(data) )
+
+        fetch("https://swapi.co/api/peoplew/1")
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                return Promise.reject(response)
+            }
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.log(error)
+            if (error.status === 404) {
+                console.log("Błąd: żądany adres nie istnieje");
+            }
+        });
+
+
+    };
 
 
 
     render() {
+
+
+     const isDuringOperation = this.state.DuringOperation;
+
         return (
             <div class="container">
                 <div class="row">
                     <div class="col">
-                        <Form onSubmit={this.handleSubmit}>
-                            <h1 className="display-3 text-center mt-5">Rejestracja</h1>
-                            <Form.Group controlId="formEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Email" name="emEmailail"  onChange={this.handleChange} value={this.state.email}  />
-                            </Form.Group>
 
-                            <Form.Group controlId="formLogin">
-                                <Form.Label>Login</Form.Label>
-                                <Form.Control type="text" placeholder="Enter login" name="UserName"  onChange={this.handleChange} value={this.state.login}  />
-                            </Form.Group>
-
-                            <Form.Group controlId="formPassword">
-                                <Form.Label>Hasło</Form.Label>
-                                <Form.Control type="password" placeholder="Password" name="Password"  onChange={this.handleChange} value={this.state.password} />
-                            </Form.Group>
-
-                            <Form.Group controlId="formPasswordRepeat">
-                                <Form.Label>Powtórz hasło</Form.Label>
-                                <Form.Control type="password" placeholder="Password" name="passwordRepeat"  onChange={this.handleChange} value={this.state.passwordRepeat}/>
-                            </Form.Group>
-
-                            <Button variant="primary" type="submit">
-                                Zarejestruj
-                        </Button>
-                        </Form>
+                    {isDuringOperation ?
+                        <Spinner animation="border" role="status">
+  <span className="sr-only">Loading...</span>
+</Spinner>  :
+                
+                             <Form onSubmit={this.handleSubmit}>
+                             <h1 className="display-3 text-center mt-5">Rejestracja</h1>
+         {this.state.InfoMessage!="" ?  <h1 className="display-5 text-center ">{this.state.InfoMessage}</h1> : null }    
+                             <Form.Group controlId="formEmail">
+                                 <Form.Label>Email</Form.Label>
+                                 <Form.Control type="email" placeholder="Email" name="Email"  onChange={this.handleChange} value={this.state.Email}  />
+                             </Form.Group>
+ 
+                             <Form.Group controlId="formLogin">
+                                 <Form.Label>Login</Form.Label>
+                                 <Form.Control type="text" placeholder="Enter login" name="Login"  onChange={this.handleChange} value={this.state.Login}  />
+                             </Form.Group>
+ 
+                             <Form.Group controlId="formPassword">
+                                 <Form.Label>Hasło</Form.Label>
+                                 <Form.Control type="password" placeholder="Password" name="Password"  onChange={this.handleChange} value={this.state.Password} />
+                             </Form.Group>
+ 
+                             <Form.Group controlId="formPasswordRepeat">
+                                 <Form.Label>Powtórz hasło</Form.Label>
+                                 <Form.Control type="password" placeholder="Password" name="ConfirmPassword"  onChange={this.handleChange} value={this.state.ConfirmPassword}/>
+                             </Form.Group>
+ 
+                             <Button variant="primary" type="submit">
+                                 Zarejestruj
+                         </Button>
+                         </Form>
+                    }
+                   
                     </div>
                 </div>
                 <Button variant="outline-primary" as={Link} to="/LoginForm" className="mt-3"  >
