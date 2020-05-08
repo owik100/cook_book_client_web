fa8fe33cfe5f38a231e1ae5d32600bc4e4831ee5
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-05-08 19:48:13 +0200

    Updates

---
+++ precache-manifest.a373f78978bea13777daff27240f6855.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f915feb6fd2a794af82dee405e483a",
+    "url": "/cook_book_client_web/index.html"
+  },
+  {
+    "revision": "143dee5fc1e8a5a27da4",
+    "url": "/cook_book_client_web/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e1d8c2c9be9121570294",
+    "url": "/cook_book_client_web/static/css/main.a0dd9446.chunk.css"
+  },
+  {
+    "revision": "143dee5fc1e8a5a27da4",
+    "url": "/cook_book_client_web/static/js/2.dd8a17f1.chunk.js"
+  },
+  {
+    "revision": "ef2a208a6ad5b8f9d5330c893d6ff0e3",
+    "url": "/cook_book_client_web/static/js/2.dd8a17f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d8c2c9be9121570294",
+    "url": "/cook_book_client_web/static/js/main.6792359e.chunk.js"
+  },
+  {
+    "revision": "25ae4682beb6e86f9174",
+    "url": "/cook_book_client_web/static/js/runtime-main.bff442ee.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/cook_book_client_web/static/media/logo.ee7cd8ed.svg"
+  }
+]);
