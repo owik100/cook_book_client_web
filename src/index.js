1af9070ec055aa3faa4c3144d6d691e8c7d0454e
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-05-09 12:16:07 +0200

    Change color to orange

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './styles/custom-btsp.scss'
 
 
 ReactDOM.render(
